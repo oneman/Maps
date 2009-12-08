13339879888ce5f57f29483116ea88f05e9ff55f
oneman <oneman@blacktower.(none)>
2009-12-07 20:34:51 -0500

    initital commit, map cordinate sql builder

---
+++ index.php
@@ -0,0 +1,4 @@
+<a href="build_coords.html">Build Cordinates from master map</a><br/>
+<a href="smallmap.php">show makers on small map</a><br/>
+<a href="medmap.php">show markers on med map</a><br/>
+<a href="mastermap.php">show markers on master map</a><br/>
